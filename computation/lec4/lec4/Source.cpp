90440aee5ade3cba64d3890a313d56b652cde13b
Krab1o <90153208+Krab1o@users.noreply.github.com>
2023-10-07 17:14:31 +0400

    lec 4 redone & lec 5

--- computation/lec4/lec4/Source.cpp
+++ computation/lec4/lec4/Source.cpp
@@ -4,26 +4,10 @@
 
 using namespace std;
 
-int main()
+const uint32_t nodeNumber = 4;
+
+double newtoneSeries(double x, vector<vector<double>>& divDiff, const vector<pair<double, double>>& functionNodes)
 {
-	vector<pair<double, double>> functionNodes =
-	{
-		make_pair(-2, -8),
-		make_pair(-1, -1),
-		make_pair(0, 0),
-		make_pair(2, 8)
-	};
-
-	vector<vector<double>> divDiff;
-	divDiff.resize(4);
-	for (int i = 0; i < 4; i++)
-	{
-		divDiff[i].resize(4 - i);
-		divDiff[i][0] = functionNodes[i].second;
-	}
-
-	double x;
-	cin >> x;
 	double ans = divDiff[0][0];
 	double currentProd = 1;
 
@@ -36,6 +20,43 @@ int main()
 		currentProd *= (x - functionNodes[j - 1].first);
 		ans += divDiff[0][j] * currentProd;
 	}
+	return ans;
+}
+
+int main()
+{
+	vector<pair<double, double>> functionNodes =
+	{
+		make_pair(-2, -8),
+		make_pair(-1, -1),
+		make_pair(0, 0),
+		make_pair(2, 8)
+	};
+
+	cout << "x:\t";
+	for (pair<double, double> val : functionNodes)
+		cout << val.first << '\t';
+	cout << '\n';
+	cout << "f:\t";
+	for (pair<double, double> val : functionNodes)
+		cout << val.second << '\t';
+	cout << '\n' << '\n';
+
+	vector<vector<double>> divDiff;
+	divDiff.resize(4);
+	for (int i = 0; i < 4; i++)
+	{
+		divDiff[i].resize(4 - i);
+		divDiff[i][0] = functionNodes[i].second;
+	}
+
+	for (int j = 1; j < 4; j++)
+	{
+		for (int i = 0; i < 4 - j; i++)
+		{
+			divDiff[i][j] = (divDiff[i + 1][j - 1] - divDiff[i][j - 1]) / (functionNodes[i + j].first - functionNodes[i].first);
+		}
+	}
 
 	for (auto el : divDiff)
 	{
@@ -45,5 +66,20 @@ int main()
 		}
 		cout << '\n';
 	}
-	cout << ans;
+	cout << '\n';
+
+	for (int i = 0; i < nodeNumber - 1; i++)
+	{
+		cout << functionNodes[i].first << '\t'
+			<< (functionNodes[i].first + functionNodes[i + 1].first) / 2 << '\t';
+	}
+	cout << functionNodes[nodeNumber - 1].first << '\n';
+
+	for (int i = 0; i < nodeNumber - 1; i++)
+	{
+		cout << newtoneSeries(functionNodes[i].first, divDiff, functionNodes) << '\t'
+			<< newtoneSeries((functionNodes[i].first + functionNodes[i + 1].first) / 2, divDiff, functionNodes) << '\t';
+	}
+
+	cout << newtoneSeries(functionNodes[nodeNumber - 1].first, divDiff, functionNodes) << '\n';
 }
---
+++ computation/lec5/Source.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const uint32_t nodeNumber = 4;
+
+/*
+	for (int i = 0; i < splineNum; i++)
+	{
+		for (int j = 0; j < 4 * splineNum; j++)
+		{
+			if (j == i)
+				matrix[i][j] = 1;
+			else
+				matrix[i][j] = 0;
+		}
+		matrix[i][4 * splineNum] = functionNodes[i].second;
+	}
+	for (int i = splineNum; i < 2 * splineNum; i++)
+	{
+		for (int j = 0; j < 4 * splineNum; j++)
+		{
+			if (j % splineNum == i % splineNum)
+				matrix[i][j] = pow((functionNodes[i % splineNum + 1].first - functionNodes[i % splineNum].first), (j / splineNum));
+			else
+				matrix[i][j] = 0;
+		}
+		matrix[i][4 * splineNum] = functionNodes[i % splineNum + 1].second;
+	}
+	for (int i = 2 * splineNum; i < 3 * splineNum - 1; i++)
+	{
+		for (int j = 0; j < 4 * splineNum; j++)
+		{
+			if (j / splineNum == 1 && j % splineNum - 1 == i % splineNum)
+				matrix[i][j] = -1;
+			else if (j % splineNum == i % splineNum)
+				matrix[i][j] = (j / splineNum) * pow((functionNodes[i % splineNum + 1].first - functionNodes[i % splineNum].first), (j / splineNum));
+			else
+				matrix[i][j] = 0;
+		}
+	}
+	*/
+
+int main()
+{
+	vector<pair<double, double>> functionNodes =
+	{
+		make_pair(-2, -8),
+		make_pair(-1, -1),
+		make_pair(0, 0),
+		make_pair(2, 8)
+	};
+
+	cout << "x:\t";
+	for (pair<double, double> val : functionNodes)
+		cout << val.first << '\t';
+	cout << '\n';
+	cout << "f:\t";
+	for (pair<double, double> val : functionNodes)
+		cout << val.second << '\t';
+	cout << '\n' << '\n';
+
+	int splineNum = nodeNumber - 1;
+
+	vector<vector<double>> matrix;
+	matrix.resize(4 * splineNum);
+	for (int i = 0; i < 4 * splineNum; i++)
+	{
+		matrix[i].resize(4 * splineNum + 1);
+	}
+
+	for (int j = 'a'; j <= 'd'; j++)
+	{
+		for (int i = 0; i < splineNum; i++)
+		{
+			cout << char(j) << '_' << i + 1 << '\t';
+		}
+	}
+	cout << 'f' << '\n';
+
+	double h = functionNodes[1].first - functionNodes[0].first;
+
+	for (int i = 0; i < splineNum; i++)
+	{
+		for (int j = 0; j < 4 * splineNum; j++)
+		{
+			if (j == i)
+				matrix[i][j] = 1;
+			else
+				matrix[i][j] = 0;
+		}
+		matrix[i][4 * splineNum] = functionNodes[i].second;
+	}
+	for (int i = splineNum; i < 2 * splineNum; i++)
+	{
+		for (int j = 0; j < 4 * splineNum; j++)
+		{
+			if (j % splineNum == i % splineNum)
+				matrix[i][j] = pow(h, (j / splineNum));
+			else
+				matrix[i][j] = 0;
+		}
+		matrix[i][4 * splineNum] = functionNodes[i % splineNum + 1].second;
+	}
+	for (int i = 2 * splineNum; i < 3 * splineNum - 1; i++)
+	{
+		for (int j = 0; j < 4 * splineNum; j++)
+		{
+			if (j / splineNum == 1 && j % splineNum - 1 == i % splineNum)
+				matrix[i][j] = -1;
+			else if (j % splineNum == i % splineNum)
+				matrix[i][j] = (j / splineNum) * pow(h, (j / splineNum));
+			else
+				matrix[i][j] = 0;
+		}
+	}
+
+	matrix[8][6] = 2;
+	matrix[9][7] = 2;
+	matrix[8][7] = -2;
+	matrix[9][8] = -2;
+	matrix[8][9] = 6 * h;
+	matrix[9][10] = 6 * h;
+	matrix[8][10] = -2;
+	matrix[8][12] = 0;
+	matrix[8][12] = 0;
+
+	matrix[10][6] = 2;
+	matrix[11][8] = 2;
+	matrix[11][11] = 6 * h;
+	matrix[10][12] = 0;
+	matrix[11][12] = 0;
+
+	for (auto str : matrix)
+	{
+		for (auto it : str)
+		{
+			cout << it << '\t';
+		}
+		cout << '\n';
+	}
+}
