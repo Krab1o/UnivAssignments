59ed6ff05a29eb7a7778a9c4f9da0217f375a7cb
Krab1o <90153208+Krab1o@users.noreply.github.com>
2024-03-22 13:32:40 +0400

    math analysis

--- sem 5/DB/Task 4/Gyms_task4_1.sql
+++ sem 5/DB/Task 4/Gyms_task4_1.sql
@@ -1,3 +1,21 @@
+/*
+
 SET statistics TIME ON
 SET statistics io ON
+
+DROP TABLE IF EXISTS Help
+
+CREATE TABLE [Help] (
+	[id] INT NOT NULL,
+	[help_string] VARCHAR(255) NOT NULL,
+	[help_int] INT NOT NULL,
+	[help_float] FLOAT NOT NULL,
+);
+
+*/
+
+SELECT * 
+FROM Clients AS Cl
+LEFT JOIN Lockers AS Lock
+ON CL.ID_client = Lock.ID_client
 
