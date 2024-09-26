a2a82a226a2fc55f23af4e23eb86188b1da0d6f0
Krab1o <bogdansils@gmail.com>
2024-09-26 13:15:33 +0400

    report python presentation

---
+++ sem 7/Script PL/main.py
@@ -0,0 +1,227 @@
+# # slide 2
+
+# class MyClass:
+#     pass  # Заглушка, указывающая на отсутствие содержимого
+
+# obj = MyClass()  # Создание экземпляра класса MyClass
+
+# class MyClass2:
+#     def __init__(self, text): # Конструктор класса
+#         print(text) # Вывод текста, переданного в конструктор
+
+# obj2 = MyClass2("Hello world") 
+
+# # slide 3
+
+# class Dog:
+#     # Атрибут класса (общий для всех объектов)
+#     species = "Canis lupus"
+    
+#     def __init__(self, name, age):
+#         # Атрибуты экземпляра (уникальны для каждого объекта)
+#         self.name = name
+#         self.age = age
+
+# # Создание экземпляров класса Dog
+# dog1 = Dog("Buddy", 3)
+# dog2 = Dog("Lucy", 5)
+
+# print(dog1.name)  # Вывод: Buddy
+# print(dog2.age)   # Вывод: 5
+# print(dog1.species)  # Вывод: Canis lupus (атрибут класса)
+
+# # slide 4
+
+# class Example:
+#     class_var = 10  # Поле класса
+
+#     def __init__(self, value):
+#         self.instance_var = value  # Поле объекта
+
+# # Создание экземпляра
+# ex = Example(5)
+
+# # Доступ к полю класса
+# print(Example.class_var)  # Вывод: 10
+
+# # Доступ к полю объекта
+# print(ex.instance_var)  # Вывод: 5
+
+# # Изменение поля класса через экземпляр
+# ex.class_var = 20  # Создаёт новое поле объекта
+# print(ex.class_var)  # Вывод: 20 (теперь это поле объекта)
+# print(Example.class_var)  # Вывод: 10 (поле класса не изменилось)
+
+# # Изменение поля класса через класс
+# Example.class_var = 30
+# print(Example.class_var)  # Вывод: 30 (поле класса изменилось)
+# print(ex.class_var)  # Вывод: 20 (поле объекта осталось прежним)
+
+# # slide 5
+
+# class Circle:
+#     pi = 3.14159
+
+#     def __init__(self, radius):
+#         self.radius = radius
+
+#     # Метод экземпляра
+#     def area(self):
+#         return Circle.pi * (self.radius ** 2)
+
+#     # Метод класса
+#     @classmethod
+#     def from_diameter(cls, diameter):
+#         radius = diameter / 2
+#         return cls(radius)
+
+#     # Статический метод
+#     @staticmethod
+#     def is_valid_radius(radius):
+#         return radius > 0
+
+# # Использование методов
+# circle1 = Circle(10)
+# print(circle1.area())  # Вывод: 314.159
+
+# circle2 = Circle.from_diameter(20)  # Создание объекта из диаметра
+# print(circle2.radius)  # Вывод: 10
+
+# print(Circle.is_valid_radius(5))  # Вывод: True (статический метод)
+
+# # slide 6
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name  # Публичный атрибут
+#         self._age = age   # Защищённый атрибут
+#         self.__password = "1234"  # Приватный атрибут
+
+#     def get_password(self):
+#         return self.__password  # Доступ к приватному атрибуту через метод
+
+# person = Person("John", 30)
+# print(person.name)       # Вывод: John (публичный атрибут)
+# print(person._age)       # Вывод: 30 (можно, но не рекомендуется)
+# # print(person.__password)  # Ошибка: нет такого атрибута
+# print(person.get_password())  # Вывод: 1234
+
+# # slide 7
+
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def speak(self):
+#         pass
+
+# class Dog(Animal):
+#     def speak(self):
+#         return f"{self.name} говорит: Гав!"
+
+# class Cat(Animal):
+#     def speak(self):
+#         return f"{self.name} говорит: Мяу!"
+
+# # Создание объектов
+# dog = Dog("Бобик")
+# cat = Cat("Мурзик")
+
+# print(dog.speak())  # Вывод: Бобик говорит: Гав!
+# print(cat.speak())  # Вывод: Мурзик говорит: Мяу!
+
+# # slide 8
+
+# class Bird:
+#     def fly(self):
+#         print("Птица летает")
+
+# class Airplane:
+#     def fly(self):
+#         print("Самолет летает")
+
+# def take_off(flying_object):
+#     flying_object.fly() # Передаём привет утиной
+#                         # типизации :)
+
+# bird = Bird()
+# airplane = Airplane()
+
+# take_off(bird)      # Вывод: Птица летает
+# take_off(airplane)  # Вывод: Самолет летает
+
+# # slide 9
+
+# from abc import ABC, abstractmethod
+
+# class Vehicle(ABC):
+#     @abstractmethod
+#     def start_engine(self):
+#         pass
+
+# class Car(Vehicle):
+#     def start_engine(self):
+#         print("Двигатель машины заведен")
+
+# class Motocycle(Vehicle):
+#     pass
+
+# car = Car()
+# car.start_engine()  # Вывод: Двигатель машины заведен
+# moto = Motocycle()  # Ошибка времени выполнения: 
+#                     # не реализован абстрактный метод
+
+# # slide 10
+
+# class A:
+#     pass
+
+# class B(A):
+#     pass
+
+# class C(A):
+#     pass
+
+# class D(B, C):
+#     pass
+
+# print(D.__mro__)  
+# # (<class '__main__.D'>, <class '__main__.B'>, 
+# # <class '__main__.C'>, <class '__main__.A'>, <class 'object'>)
+# print(D.mro())    
+# # [<class '__main__.D'>, <class '__main__.B'>, 
+# # <class '__main__.C'>, <class '__main__.A'>, <class 'object'>]
+
+# # slide 11
+
+# class Animal:
+#     pass
+
+# class Dog(Animal):
+#     pass
+
+# dog = Dog()
+
+# print(isinstance(dog, Dog))      # True
+# print(isinstance(dog, Animal))   # True
+# print(issubclass(Dog, Animal))   # True
+# print(issubclass(Animal, Dog))   # False
+
+# slide 12 (PROPERTY)
+
+# slide 13
+
+def multiply(a, b):
+    """
+    Умножает два числа.
+
+    Args:
+        a (int, float): Первое число.
+        b (int, float): Второе число.
+
+    Returns:
+        int, float: Произведение чисел.
+    """
+    return a * b
+
+print(multiply.__doc__)
--- sem 7/Security/task1/main.py
+++ sem 7/Security/task1/main.py
@@ -1,48 +1,74 @@
-# import os
-
-# # path = "task1/files/file1.txt"
-# start_path = os.getcwd()
-# print(os.walk)
-# n = 0
-
-# with open(start_path, 'rb') as file:
-#     while True:
-#         chars = file.read(2)
-#         if not chars:
-#             break
-#         print(chars)
-#         n += 2
-
 import os
-import sys
+import struct
 
-walk_dir = os.
+HASH_FILE_NAME = "hashes.txt"
 
-print('walk_dir = ' + os.getcwd())
+def calculate_hash(file_path):
+    """Вычисляет хэш-сумму файла по описанному алгоритму."""
+    hash_value = 0
+    try:
+        with open(file_path, 'rb') as file:
+            while chunk := file.read(2):  # Читаем файл по 2 байта (16 бит)
+                if len(chunk) < 2:
+                    chunk = chunk.ljust(2, b'\0')  # Дополняем нулями, если не хватает
+                value = struct.unpack('H', chunk)[0]  # Преобразуем в 16-битное значение
+                hash_value ^= value  # Складываем по XOR
+    except IOError as e:
+        print(f"Ошибка чтения файла {file_path}: {e}")
+    return hash_value
 
-# If your current working directory may change during script execution, it's recommended to
-# immediately convert program arguments to an absolute path. Then the variable root below will
-# be an absolute path as well. Example:
-# walk_dir = os.path.abspath(walk_dir)
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+def save_hashes(directory):
+    """Сохраняет хэш-суммы всех файлов в указанном каталоге."""
+    hashes = {}
+    for root, _, files in os.walk(directory):
+        for file_name in files:
+            if file_name == HASH_FILE_NAME:
+                continue  # Пропускаем файл с хэш-суммами
+            file_path = os.path.join(root, file_name)
+            file_hash = calculate_hash(file_path)
+            relative_path = os.path.relpath(file_path, directory)
+            hashes[relative_path] = file_hash
 
-for root, subdirs, files in os.walk(walk_dir):
-    print('--\nroot = ' + root)
-    list_file_path = os.path.join(root, 'my-directory-list.txt')
-    print('list_file_path = ' + list_file_path)
+    # Сохраняем хэш-суммы в файл
+    with open(os.path.join(directory, HASH_FILE_NAME), 'w') as hash_file:
+        for file_path, file_hash in hashes.items():
+            hash_file.write(f"{file_path}:{file_hash}\n")
 
-    with open(list_file_path, 'wb') as list_file:
-        for subdir in subdirs:
-            print('\t- subdirectory ' + subdir)
+def check_integrity(directory):
+    """Проверяет целостность файлов в указанном каталоге."""
+    hash_file_path = os.path.join(directory, HASH_FILE_NAME)
+    if not os.path.exists(hash_file_path):
+        print("Файл с хэш-суммами не найден.")
+        return
 
-        for filename in files:
-            file_path = os.path.join(root, filename)
+    # Загружаем сохраненные хэш-суммы
+    saved_hashes = {}
+    with open(hash_file_path, 'r') as hash_file:
+        for line in hash_file:
+            file_path, file_hash = line.strip().split(':')
+            saved_hashes[file_path] = int(file_hash)
 
-            print('\t- file %s (full path: %s)' % (filename, file_path))
+    # Проверяем текущие хэш-суммы
+    for root, _, files in os.walk(directory):
+        for file_name in files:
+            if file_name == HASH_FILE_NAME:
+                continue  # Пропускаем файл с хэш-суммами
+            file_path = os.path.join(root, file_name)
+            relative_path = os.path.relpath(file_path, directory)
+            current_hash = calculate_hash(file_path)
 
-            with open(file_path, 'rb') as f:
-                f_content = f.read()
-                list_file.write(('The file %s contains:\n' % filename).encode('utf-8'))
-                list_file.write(f_content)
-                list_file.write(b'\n')
+            if relative_path not in saved_hashes:
+                print(f"Новый файл обнаружен: {relative_path}")
+            elif current_hash != saved_hashes[relative_path]:
+                print(f"Файл изменен: {relative_path}")
+                print(f"  Старый хэш: {saved_hashes[relative_path]}, Новый хэш: {current_hash}")
     
+    # Проверяем удаленные файлы
+    for saved_file in saved_hashes:
+        if not os.path.exists(os.path.join(directory, saved_file)):
+            print(f"Файл удален: {saved_file}")
+
+# Пример использования
+# directory = "/path/to/your/directory"
+# save_hashes(directory)  # Сохраняем хэш-суммы
+# check_integrity(directory)  # Проверяем целостность
